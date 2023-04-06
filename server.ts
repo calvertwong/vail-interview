407b01900de8302014a1f43096c75459b5f4a2bd
Calvert <wongcalvert@gmail.com>
2023-04-06 12:52:25 -0700

    Added swagger

--- server.ts
+++ server.ts
@@ -1,10 +1,13 @@
 import express from "express";
 import { postRouter } from "./routes/post/ping";
 import { expressParser } from "./middlewares/ExpressParser";
+import swaggerUi from "swagger-ui-express"
+import swaggerDocument from "./swagger.json"
 
 export const server: express.Application = express();
 
 expressParser();
+server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 server.use("/api", postRouter);
 
